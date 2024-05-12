758d4158de648121a20ddfde3caa616476eefab4
Emiliano Del Arco <emiliano.delarco@educacion.local>
2024-05-12 20:22:38 -0300

    clase 8 - room

---
+++ app/src/main/java/com/example/aprendeprogramando/vistas/WeatherScreen.kt
@@ -0,0 +1,13 @@
+package com.example.aprendeprogramando.vistas
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun WeatherScreen() {
+    Column {
+        Text("Clima")
+        // Agregar código para mostrar información del clima
+    }
+}
